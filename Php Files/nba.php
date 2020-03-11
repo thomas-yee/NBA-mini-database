deb3b651980aab2b9ddb472386b22fcde442194e
firestar555 <thomasyee40@gmail.com>
2020-03-11 13:22:43 -0600

    Basic php file with a drop down menu for teams

---
+++ Php Files/nba.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h1>NBA Mini-Database</h1>
+Team List:
+<select name="Team">
+    <option value="raptors">Toronto Raptors</option>
+    <option value="lakers">Los Angeles Lakers</option>
+</select>
+
+</body>
+</html>
