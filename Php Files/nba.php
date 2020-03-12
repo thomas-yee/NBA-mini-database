b5e5953e91decacaaf63efb1a0729fbbc97cd2a3
firestar555 <thomasyee40@gmail.com>
2020-03-11 21:25:29 -0600

    Added navigation and nba logo to front end

--- Php Files/nba.php
+++ Php Files/nba.php
@@ -1,40 +1,59 @@
+<!DOCTYPE html>
 <html>
-<body>
-
-<h1>NBA Mini-Database</h1>
-Team List:
-<select name="Team">
-    <option value="hawks">Atlanta Hawks</option>
-    <option value="celtics">Boston Celtics</option>
-    <option value="nets">Brooklyn Nets</option>
-    <option value="hornets">Charlotte Hornets</option>
-    <option value="bulls">Chicago Bulls</option>
-    <option value="cavaliers">Cleveland Cavaliers</option>
-    <option value="mavericks">Dallas Mavericks</option>
-    <option value="nuggets">Denver Nuggets</option>
-    <option value="pistons">Detroit Pistons</option>
-    <option value="warriors">Golden State Warriors</option>
-    <option value="rockets">Houston Rockets</option>
-    <option value="pacers">Indiana Pacers</option>
-    <option value="clippers">Los Angeles Clippers</option>
-    <option value="lakers">Los Angeles Lakers</option>
-    <option value="grizzlies">Memphis Grizzlies</option>
-    <option value="heat">Miami Heat</option>
-    <option value="bucks">Milwaukee Bucks</option>
-    <option value="timberwolves">Minnesota Timberwolves</option>
-    <option value="pelicans">New Orleans Pelicans</option>
-    <option value="knicks">New York Knicks/option>
-    <option value="thunder">Oklahoma City Thunder</option>
-    <option value="magic">Orlando Magic</option>
-    <option value="76ers">Philadelphia 76ers</option>
-    <option value="suns">Phoenix Suns</option>
-    <option value="blazers">Portland Trail Blazers/option>
-    <option value="kings">Sacramento Kings</option>
-    <option value="spurs">San Antonio Spurs</option>
-    <option value="raptors">Toronto Raptors</option>
-    <option value="jazz">Utah Jazz</option>
-    <option value="wizards">Washington Wizards</option>
-</select>
-
-</body>
+    <head>
+        <!--Link css file -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>NBA Mini-Database</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="row">
+                    <h1>NBA Mini-Database</h1>
+                    <img src="resources/nba-logo-png-transparent.png" alt="Nba Logo" class="logo">
+                    <ul class="main-nav"> <!--ul means unordered list-->
+                        <li><a href="a">Teams</a></li>
+                        <li><a href="a">Games</a></li>
+                        <li><a href="a">Players</a></li>
+                        <li><a href="a">Contact us</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <section class = "team-list">
+            Team List:
+            <select name="Team">
+                <option value="hawks">Atlanta Hawks</option>
+                <option value="celtics">Boston Celtics</option>
+                <option value="nets">Brooklyn Nets</option>
+                <option value="hornets">Charlotte Hornets</option>
+                <option value="bulls">Chicago Bulls</option>
+                <option value="cavaliers">Cleveland Cavaliers</option>
+                <option value="mavericks">Dallas Mavericks</option>
+                <option value="nuggets">Denver Nuggets</option>
+                <option value="pistons">Detroit Pistons</option>
+                <option value="warriors">Golden State Warriors</option>
+                <option value="rockets">Houston Rockets</option>
+                <option value="pacers">Indiana Pacers</option>
+                <option value="clippers">Los Angeles Clippers</option>
+                <option value="lakers">Los Angeles Lakers</option>
+                <option value="grizzlies">Memphis Grizzlies</option>
+                <option value="heat">Miami Heat</option>
+                <option value="bucks">Milwaukee Bucks</option>
+                <option value="timberwolves">Minnesota Timberwolves</option>
+                <option value="pelicans">New Orleans Pelicans</option>
+                <option value="knicks">New York Knicks/option>
+                <option value="thunder">Oklahoma City Thunder</option>
+                <option value="magic">Orlando Magic</option>
+                <option value="76ers">Philadelphia 76ers</option>
+                <option value="suns">Phoenix Suns</option>
+                <option value="blazers">Portland Trail Blazers/option>
+                <option value="kings">Sacramento Kings</option>
+                <option value="spurs">San Antonio Spurs</option>
+                <option value="raptors">Toronto Raptors</option>
+                <option value="jazz">Utah Jazz</option>
+                <option value="wizards">Washington Wizards</option>
+            </select>
+        </section>
+    </body>
 </html>
