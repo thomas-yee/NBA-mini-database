e0a713b812f6d09039b911ba19d849b75847c618
firestar555 <thomasyee40@gmail.com>
2020-03-16 20:14:57 -0600

    added connection to mysql

--- Php Files/nba.php
+++ Php Files/nba.php
@@ -1,4 +1,20 @@
-<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+//Create the connection
+$conn = new mysqli($servername, $username, $password);
+
+//Check connection
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//if it is connected successfully --> website will show up
+//echo "Connected successfully";
+?>
+
+
 <html>
     <head>
         <!--Link css file -->
