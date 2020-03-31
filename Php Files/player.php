feeb2d77f596f2dbd71057c304954927ad3c0dac
firestar555 <thomasyee40@gmail.com>
2020-03-30 21:34:58 -0600

    Columns will sort in the opposite direction when clicked again

--- Php Files/player.php
+++ Php Files/player.php
@@ -29,36 +29,115 @@
     <section class = "display-stats">
         <div id = "row">
             <table>
+                <!--Column headers-->
+                <tr>
+                    <?php
+                        //If order == DESC is true then use DESC otherwise use ASC
+                        //Since order hasnt been set, it will default ASC
+                        //After clicked, it will be set to DESC since statement is true
+                        //isset() used to get rid of error
+                        $sort_order = isset($_GET['order']) && $_GET['order'] == 'DESC' ? 'DESC' : 'ASC';
+                        //if sort_order is ASC, it will set asc_or_desc to DESC
+                        //After clicked, it will be sent to ASC since statement will be false
+                        $asc_or_desc = $sort_order == 'ASC' ? 'DESC' : 'ASC';
+                    ?>
+                    <!-- Column Links, when clicked, calls the same page but changes the
+                    variable ?sort based on the column the user wants to sort by 
+                    order variable set based on whether the column is sorted -->
+                    <th><a href='player.php?sort=firstName&order=<?php echo $asc_or_desc;?>'>First Name</a></th>
+                    <th><a href='player.php?sort=lastName&order=<?php echo $asc_or_desc;?>'>Last Name</a></th>
+                    <th><a href='player.php?sort=season&order=<?php echo $asc_or_desc;?>'>Season</a></th>
+                    <th><a href='player.php?sort=gamesPlayed&order=<?php echo $asc_or_desc;?>'>Games Played</a></th>
+                    <th><a href='player.php?sort=gamesStarted&order=<?php echo $asc_or_desc;?>'>Games Started</a></th>
+                    <th><a href='player.php?sort=min&order=<?php echo $asc_or_desc;?>'>MIN</a></th>
+                    <th><a href='player.php?sort=fg&order=<?php echo $asc_or_desc;?>'>FG</a></th>
+                    <th><a href='player.php?sort=fg%&order=<?php echo $asc_or_desc;?>'>FG%</a></th>
+                    <th><a href='player.php?sort=3pt&order=<?php echo $asc_or_desc;?>'>3PT</a></th>
+                    <th><a href='player.php?sort=3pt%&order=<?php echo $asc_or_desc;?>'>3PT%</a></th>
+                    <th><a href='player.php?sort=ft&order=<?php echo $asc_or_desc;?>'>FT</a></th>
+                    <th><a href='player.php?sort=ft%&order=<?php echo $asc_or_desc;?>'>FT%</a></th>
+                    <th><a href='player.php?sort=or&order=<?php echo $asc_or_desc;?>'>OR</a></th>
+                    <th><a href='player.php?sort=dr&order=<?php echo $asc_or_desc;?>'>DR</a></th>
+                    <th><a href='player.php?sort=reb&order=<?php echo $asc_or_desc;?>'>REB</a></th>
+                    <th><a href='player.php?sort=ast&order=<?php echo $asc_or_desc;?>'>AST</a></th>
+                    <th><a href='player.php?sort=blk&order=<?php echo $asc_or_desc;?>'>BLK</a></th>
+                    <th><a href='player.php?sort=stl&order=<?php echo $asc_or_desc;?>'>STL</a></th>
+                    <th><a href='player.php?sort=pf&order=<?php echo $asc_or_desc;?>'>PF</a></th>
+                    <th><a href='player.php?sort=to&order=<?php echo $asc_or_desc;?>'>TO</a></th>
+                    <th><a href='player.php?sort=ppg&order=<?php echo $asc_or_desc;?>'>PPG</a></th>
+                </tr>
             <?php
-                $sql = "SELECT * FROM player_stats;";
+                //Original SQL query to show all stats
+                $sql = "SELECT * FROM player_stats\n";
+                //If a column is clicked, sort will be added to the query
+                if (isset($_GET['sort'])) {
+                    if ($_GET['sort'] == 'firstName') {
+                        $sql .= " ORDER BY FirstName $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'lastName') {
+                        $sql .= " ORDER BY LastName $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'season') {
+                        $sql .= " ORDER BY Season $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'gamesPlayed') {
+                        $sql .= " ORDER BY GP $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'gamesStarted') {
+                        $sql .= " ORDER BY GS $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'min') {
+                        $sql .= " ORDER BY MIN $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'fg') {
+                        $sql .= " ORDER BY FG $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'fg%') {
+                        $sql .= " ORDER BY `FG%` $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == '3pt') {
+                        $sql .= " ORDER BY 3PT $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == '3pt%') {
+                        $sql .= " ORDER BY `3P%` $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'ft') {
+                        $sql .= " ORDER BY FT $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'ft%') {
+                        $sql .= " ORDER BY `FT%` $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'or') {
+                        $sql .= " ORDER BY `OR` $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'dr') {
+                        $sql .= " ORDER BY DR $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'reb') {
+                        $sql .= " ORDER BY REB $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'ast') {
+                        $sql .= " ORDER BY AST $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'blk') {
+                        $sql .= " ORDER BY BLK $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'stl') {
+                        $sql .= " ORDER BY STL $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'pf') {
+                        $sql .= " ORDER BY PF $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'to') {
+                        $sql .= " ORDER BY `TO` $asc_or_desc";
+                    }
+                    elseif ($_GET['sort'] == 'ppg') {
+                        $sql .= " ORDER BY PPG $asc_or_desc";
+                    }
+                }
                 $result = mysqli_query($conn, $sql);
                 $resultCheck = mysqli_num_rows($result); //Check for a result above 0
-
                 if ($resultCheck > 0) {
-                    //Column headers
-                    echo "<tr>";
-                    echo "<th>First Name</th>";
-                    echo "<th>Last Name</th>";
-                    echo "<th>Season</th>";
-                    echo "<th>Games Played</th>";
-                    echo "<th>Games Started</th>";
-                    echo "<th>MIN</th>";
-                    echo "<th>FG</th>";
-                    echo "<th>FG%</th>";
-                    echo "<th>3PT</th>";
-                    echo "<th>3PT%</th>";
-                    echo "<th>FT</th>";
-                    echo "<th>FT%</th>";
-                    echo "<th>OR</th>";
-                    echo "<th>DR</th>";
-                    echo "<th>REB</th>";
-                    echo "<th>AST</th>";
-                    echo "<th>BLK</th>";
-                    echo "<th>STL</th>";
-                    echo "<th>PF</th>";
-                    echo "<th>TO</th>";
-                    echo "<th>PPG</th>";
-                    echo "</tr>";
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo "<tr><td>". $row['FirstName'].
                         "</td><td>". $row['LastName']. 
